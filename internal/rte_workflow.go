683697d5724c9bf6ff72de4d4a3e2abf830187b7
Jorick Caberio <jorick.caberio@paymaya.com>
2021-04-13 18:41:43 +0800

    refactor rte workflow

--- internal/rte_workflow.go
+++ internal/rte_workflow.go
@@ -9,58 +9,78 @@ type RteWorkflow struct {
 }
 
 type Item struct {
-	App               string `json:"app"`
-	BaseInputSchemaID string `json:"baseInputSchemaId"`
-	Config            Config `json:"config,omitempty"`
-	CreatedAt         int64  `json:"createdAt"`
-	CreatedBy         string `json:"createdBy"`
-	Desc              string `json:"desc"`
-	ID                string `json:"id"`
-	Name              string `json:"name"`
-	OutcomeConfig     struct {
-		Outcomes []struct {
-			Type              string      `json:"@type"`
-			DefaultValue      interface{} `json:"defaultValue"`
-			ID                string      `json:"id"`
-			Label             string      `json:"label"`
-			CategoricalValues []string    `json:"categoricalValues,omitempty"`
-		} `json:"outcomes"`
-	} `json:"outcomeConfig"`
-	Ownership struct {
-		CurrentUserRights int `json:"currentUserRights"`
-		Groups            struct {
-			JrmgtUwhFazNKB7BVLZFJw int `json:"jrmgtUwhFazNKB7bVLZFJw"`
-		} `json:"groups"`
-		UserVisibleGroups []interface{} `json:"userVisibleGroups"`
-		Visibility        string        `json:"visibility"`
-	} `json:"ownership"`
-	UpdatedAt int64  `json:"updatedAt"`
-	UpdatedBy string `json:"updatedBy"`
+	App               string        `json:"app"`
+	Baseinputschemaid string        `json:"baseInputSchemaId"`
+	Config            Config        `json:"config"`
+	Createdat         int64         `json:"createdAt"`
+	Createdby         string        `json:"createdBy"`
+	Desc              string        `json:"desc"`
+	ID                string        `json:"id"`
+	Name              string        `json:"name"`
+	Outcomeconfig     Outcomeconfig `json:"outcomeConfig"`
+	Ownership         Ownership     `json:"ownership"`
+	Updatedat         int64         `json:"updatedAt"`
+	Updatedby         string        `json:"updatedBy"`
 }
-
-type Config struct {
-	ActionHandlers      []interface{}     `json:"actionHandlers"`
-	Actions             []Action          `json:"actions"`
-	AdvancedProperties  struct{}          `json:"advancedProperties"`
-	Connections         []interface{}     `json:"connections"`
-	Elements            []WorkflowElement `json:"elements"`
-	EventStorageEnabled bool              `json:"eventStorageEnabled"`
-	PartitionKeys       []interface{}     `json:"partitionKeys"`
-}
-
-type WorkflowElement struct {
-	Type          string        `json:"@type"`
-	Configuration struct{}      `json:"configuration"`
-	DependencyID  string        `json:"dependencyId"`
-	Desc          string        `json:"desc"`
-	ID            string        `json:"id"`
-	InputMapping  struct{}      `json:"inputMapping"`
-	Metadata      string        `json:"metadata"`
-	OutcomeIds    []interface{} `json:"outcomeIds"`
-	OutputMapping struct{}      `json:"outputMapping"`
-}
-
-type Action struct {
+type Actions struct {
 	Name   string        `json:"name"`
 	Params []interface{} `json:"params"`
+}
+type Advancedproperties struct {
+    Numbereventstoragesplits string `json:"numberEventStorageSplits"`
+}
+type Filter struct {
+	Fields   []string `json:"fields"`
+	Template string   `json:"template"`
+}
+type Connections struct {
+	Filter   Filter `json:"filter,omitempty"`
+	ID       string `json:"id"`
+	Sinkid   string `json:"sinkId"`
+	Sourceid string `json:"sourceId"`
+}
+type Configuration struct {
+}
+type Inputmapping struct {
+}
+type Outputmapping struct {
+}
+type Elements struct {
+	Type          string        `json:"@type"`
+	Configuration Configuration `json:"configuration"`
+	Dependencyid  string        `json:"dependencyId"`
+	Desc          string        `json:"desc"`
+	ID            string        `json:"id"`
+	Inputmapping  Inputmapping  `json:"inputMapping"`
+	Metadata      string        `json:"metadata"`
+	Outcomeids    []interface{} `json:"outcomeIds"`
+	Outputmapping Outputmapping `json:"outputMapping"`
+}
+type Config struct {
+	Actionhandlers      []interface{}      `json:"actionHandlers"`
+	Actions             []Actions          `json:"actions"`
+	Advancedproperties  Advancedproperties `json:"advancedProperties"`
+	Connections         []Connections      `json:"connections"`
+	Elements            []Elements         `json:"elements"`
+	Eventstorageenabled bool               `json:"eventStorageEnabled"`
+	Partitionkeys       []string           `json:"partitionKeys"`
+}
+type Outcomes struct {
+	Type              string   `json:"@type"`
+	Defaultvalue      bool     `json:"defaultValue"`
+	ID                string   `json:"id"`
+	Label             string   `json:"label"`
+	Categoricalvalues []string `json:"categoricalValues,omitempty"`
+}
+type Outcomeconfig struct {
+	Outcomes []Outcomes `json:"outcomes"`
+}
+type Groups struct {
+	Jrmgtuwhfaznkb7Bvlzfjw int `json:"jrmgtUwhFazNKB7bVLZFJw"`
+}
+type Ownership struct {
+	Currentuserrights int           `json:"currentUserRights"`
+	Groups            Groups        `json:"groups"`
+	Uservisiblegroups []interface{} `json:"userVisibleGroups"`
+	Visibility        string        `json:"visibility"`
 }
